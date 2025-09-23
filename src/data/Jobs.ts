26718984815e931acbc8a2a01e0c0a40be21940e
JesusxAguirre <99jesus.aguirre@gmail.com>
2025-09-23 13:04:11 +0200

    add iptv app setup project

--- src/data/Jobs.ts
+++ src/data/Jobs.ts
@@ -43,23 +43,23 @@ interface WorkExperience {
  * @property {boolean} currentJob - A flag indicating if the job is the current one.
  */
 const workExperience: WorkExperience[] = [
-    {
-        title: "Freelancer Flutter Senior Developer",
-        startDate: "2025-07-5"
-        ,
-        endDate: "2025-09-30",
-        company: "Civita Technologies",
-        location: "Dublin, Ireland",
-        description: "Developed and maintained mobile app using Flutter.",
-        goals: [
-            "Developed a booking mobile app from scratch using Flutter + Bloc, achieving a scalable and maintainable architecture that improved development speed by 30%.",
-            "Implemented secure authentication with JWT, Google, Apple, and Email Sign-In, including password recovery via deep links, increasing user login success rate by 25%.",
-            "Integrated Google Maps SDK with real-time navigation, custom markers (linked to backend venues), and route drawing, enhancing geolocation accuracy and boosting user engagement by 40%",
-            "Built an in-app AI-powered chat (backend-driven) with WebSockets, reducing user support requests by 20%",
-            "Automated delivery pipelines with Codemagic CI/CD, successfully publishing the app to App Store and Play Store with zero critical release issues",
-        ],
-        currentJob: false,
-    },
+    // {
+    //     title: "Freelancer Flutter Senior Developer",
+    //     startDate: "2025-07-5"
+    //     ,
+    //     endDate: "2025-09-30",
+    //     company: "Civita Technologies",
+    //     location: "Dublin, Ireland",
+    //     description: "Developed and maintained mobile app using Flutter.",
+    //     goals: [
+    //         "Developed a booking mobile app from scratch using Flutter + Bloc, achieving a scalable and maintainable architecture that improved development speed by 30%.",
+    //         "Implemented secure authentication with JWT, Google, Apple, and Email Sign-In, including password recovery via deep links, increasing user login success rate by 25%.",
+    //         "Integrated Google Maps SDK with real-time navigation, custom markers (linked to backend venues), and route drawing, enhancing geolocation accuracy and boosting user engagement by 40%",
+    //         "Built an in-app AI-powered chat (backend-driven) with WebSockets, reducing user support requests by 20%",
+    //         "Automated delivery pipelines with Codemagic CI/CD, successfully publishing the app to App Store and Play Store with zero critical release issues",
+    //     ],
+    //     currentJob: false,
+    // },
     {
         title: "Senior Mobile Engineer",
         startDate: "2024-04-1",
@@ -68,12 +68,12 @@ const workExperience: WorkExperience[] = [
         location: "Venezuela",
         description: "Developed and maintained mobile app applications using Flutter.",
         goals: [
-            "Designed and developed an offline-first agricultural monitoring app using Flutter + BLoC, enabling full functionality without internet and seamless background sync with Firestore, reducing data loss in low-connectivity rural areas by 40%.",
-            "Integrated Mapbox with offline maps, interactive layers for farms and sublots (polygons), and geolocation services, improving field monitoring accuracy by 35%.",
-            "Developed push notifications with Firebase Cloud Messaging (FCM) to deliver real-time alerts on crop activities and monitoring sessions, increasing user engagement by 25%.",
+            "Enabled real-time agricultural monitoring for 20 farmers across 1,000+ hectares by building a Flutter + BLoC offline-first app with Firestore sync, reducing report delivery time from 3 weeks to 1–3 days",
+            "Implemented offline maps and interactive farm/sublot layers with Mapbox, allowing precise location tracking of activities and monitoring sessions",
+            "Delivered instant crop alerts and activity updates via Firebase FCM, enabling farmers to make timely decisions on labor and resource allocation",
+            "Designed modular Activities and Monitoring workflows, providing a clear, structured process for planning and tracking crop cycles.",
             "Automated CI/CD workflows with Codemagic, streamlining delivery pipelines and ensuring consistent releases on App Store and Play Store",
-            "Automated delivery pipelines with Codemagic CI/CD, successfully publishing the app to App Store and Play Store with zero critical release issue",
-            "Optimized app performance and offline caching, achieving smoother navigation and a 20% reduction in loading times"
+            "Created multi-farm management and producer switching features, giving agronomists and farmers immediate access to all relevant farms and plots",
         ],
         currentJob: false,
     },
@@ -85,26 +85,24 @@ const workExperience: WorkExperience[] = [
         location: "Venezuela",
         description: "Developed and maintained backend APIS with microservices arquitecture",
         goals: [
-            "Developed a livestock management mobile app in Flutter, implementing a local database with Drift + SQLite and synchronization via REST APIs, improving data availability for field managers by 35%",
-            "Implemented state management with GetX and secure authentication with JWT, ensuring robust offline-to-online user sessions",
-            "Optimized network layer using Dio, reducing API communication errors by 20% in low-connectivity scenarios",
-            "Designed and deployed backend microservices with NestJS + NATS, containerized with Docker and deployed on AWS, enabling scalable and fault-tolerant data pipelines",
-
+            "Built a livestock management app with Flutter + Drift/SQLite, improving field data availability by 35%.",
+            "Developed NestJS microservices with NATS, Dockerized for AWS, enabling scalable pipelines.",
+            "Strengthened network reliability with Dio, reducing API errors by 20% in poor connectivity",
         ],
         currentJob: false,
     },
-    {
-        title: "Jr. Full-stack Engineer",
-        startDate: "2023-06-1",
-        endDate: "2023-09-1",
-        company: "Intercom Servicios",
-        location: "Venezuela",
-        description: "Developed and maintained backend APIS with microservices arquitecture",
-        goals: [
-            "I applied the Abstract Factory design pattern to standardize and optimize commands across different OLT systems, enhancing operational efficiency",
-            "I implemented tailored CRM solutions for Internet service providers, integrating functionalities with Google Maps for accurate fiber optic service"
-        ],
-        currentJob: false,
-    },
+    // {
+    //     title: "Jr. Full-stack Engineer",
+    //     startDate: "2023-06-1",
+    //     endDate: "2023-09-1",
+    //     company: "Intercom Servicios",
+    //     location: "Venezuela",
+    //     description: "Developed and maintained backend APIS with microservices arquitecture",
+    //     goals: [
+    //         "I applied the Abstract Factory design pattern to standardize and optimize commands across different OLT systems, enhancing operational efficiency",
+    //         "I implemented tailored CRM solutions for Internet service providers, integrating functionalities with Google Maps for accurate fiber optic service"
+    //     ],
+    //     currentJob: false,
+    // },
 ];
 export default workExperience;
